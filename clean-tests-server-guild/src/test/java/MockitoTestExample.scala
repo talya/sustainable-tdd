1c625761a50fcdc3a16d81d40497e7695b3195e5
talyag <talyag@wix.com>
2017-08-13 11:47:22 +0300

    jmock fails on same code since it IS strict

--- clean-tests-server-guild/src/test/java/MockitoTestExample.scala
+++ clean-tests-server-guild/src/test/java/MockitoTestExample.scala
@@ -1,5 +1,6 @@
 import java.util.UUID
 
+import com.wixpress.common.specs2.JMock
 import org.specs2.mock.Mockito
 import org.specs2.mutable.SpecificationWithJUnit
 import org.specs2.specification.Scope
@@ -11,6 +12,28 @@ class MockitoTestExample extends SpecificationWithJUnit with Mockito {
     "return only provisioned tpa and generate events for them" in new Context {
       metaSiteTpaCollector.collectProvisionedTpas(Seq(provisionedTpa, templateTpa)) must contain(exactly(provisionedTpa.id))
       there was one(eventGenerator).generateProvisionedEvent(provisionedTpa.id)
+    }
+  }
+
+  trait Context extends Scope {
+    val eventGenerator = mock[EventGenerator]
+    val metaSiteTpaCollector = new MetaSiteTpaCollector(eventGenerator)
+
+    val provisionedTpa = TpaInstance(id = UUID.randomUUID, TpaInstance.PROVISIONED)
+    val templateTpa = TpaInstance(id = UUID.randomUUID, TpaInstance.TEMPLATE)
+  }
+
+}
+
+class JMockTestExample extends SpecificationWithJUnit with JMock {
+
+  "MetaSiteTpaCollector" should {
+
+    "return only provisioned tpa and generate events for them" in new Context {
+      checking {
+        oneOf(eventGenerator).generateProvisionedEvent(provisionedTpa.id)
+      }
+      metaSiteTpaCollector.collectProvisionedTpas(Seq(provisionedTpa, templateTpa)) must contain(exactly(provisionedTpa.id))
     }
   }
 
