a15fbebd91f9436e36480e1a63b8d31e169e418d
talyag <talyag@wix.com>
2017-08-13 11:50:04 +0300

    remove mockito

--- clean-tests-server-guild/src/test/java/MockitoTestExample.scala
+++ clean-tests-server-guild/src/test/java/MockitoTestExample.scala
@@ -1,29 +1,8 @@
 import java.util.UUID
 
 import com.wixpress.common.specs2.JMock
-import org.specs2.mock.Mockito
 import org.specs2.mutable.SpecificationWithJUnit
 import org.specs2.specification.Scope
-
-class MockitoTestExample extends SpecificationWithJUnit with Mockito {
-
-  "MetaSiteTpaCollector" should {
-
-    "return only provisioned tpa and generate events for them" in new Context {
-      metaSiteTpaCollector.collectProvisionedTpas(Seq(provisionedTpa, templateTpa)) must contain(exactly(provisionedTpa.id))
-      there was one(eventGenerator).generateProvisionedEvent(provisionedTpa.id)
-    }
-  }
-
-  trait Context extends Scope {
-    val eventGenerator = mock[EventGenerator]
-    val metaSiteTpaCollector = new MetaSiteTpaCollector(eventGenerator)
-
-    val provisionedTpa = TpaInstance(id = UUID.randomUUID, TpaInstance.PROVISIONED)
-    val templateTpa = TpaInstance(id = UUID.randomUUID, TpaInstance.TEMPLATE)
-  }
-
-}
 
 class JMockTestExample extends SpecificationWithJUnit with JMock {
 
